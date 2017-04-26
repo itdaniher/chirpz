0bcd14f8cd6321c6ec874184b28f0a966516b3f1
Ian Daniher <itdaniher@gmail.com>
2017-04-26 13:20:55 -0400

    python3 and less self-hatred

--- chirpz.py
+++ chirpz.py
@@ -13,51 +13,51 @@ import numpy as np
 
 def chirpz(x, A, W, M):
 
-	"""Compute the chirp z-transform.
-	The discrete z-transform,
-	X(z) = \sum_{n=0}^{N-1} x_n z^{-n}
-	is calculated at M points,
-	z_k = AW^-k, k = 0,1,...,M-1
-	for A and W complex, which gives
-	X(z_k) = \sum_{n=0}^{N-1} x_n z_k^{-n}
-	"""
+    """Compute the chirp z-transform.
+    The discrete z-transform,
+    X(z) = \sum_{n=0}^{N-1} x_n z^{-n}
+    is calculated at M points,
+    z_k = AW^-k, k = 0,1,...,M-1
+    for A and W complex, which gives
+    X(z_k) = \sum_{n=0}^{N-1} x_n z_k^{-n}
+    """
 
-	""" DFT:
-	A = 1 (radius)
-	a = cmath.exp(2j * pi * f1/Fs)
-	M = N (len output = len input)
-	W = e ^ (-j*tau/N) (curve spans 0 to 2pi)
-	w = cmath.exp(-2j * pi * (f2-f1) / ((m-1)*Fs))
-	"""
+    """ DFT:
+    A = 1 (radius)
+    a = cmath.exp(2j * pi * f1/Fs)
+    M = N (len output = len input)
+    W = e ^ (-j*tau/N) (curve spans 0 to 2pi)
+    w = cmath.exp(-2j * pi * (f2-f1) / ((m-1)*Fs))
+    """
 
-	A = np.complex(A)
-	W = np.complex(W)
-	if np.issubdtype(np.complex,x.dtype) or np.issubdtype(np.float,x.dtype):
-		dtype = x.dtype
-	else:
-		dtype = float
+    A = np.complex(A)
+    W = np.complex(W)
+    if np.issubdtype(np.complex,x.dtype) or np.issubdtype(np.float,x.dtype):
+        dtype = x.dtype
+    else:
+        dtype = float
+    M = int(M)
+    x = np.asarray(x,dtype=np.complex)
 
-	x = np.asarray(x,dtype=np.complex)
+    N = x.size
+    # smallest power of two appropriate for fast convolution
+    L = int(2**np.ceil(np.log2(M+N-1)))
 
-	N = x.size
-	# smallest power of two appropriate for fast convolution
-	L = int(2**np.ceil(np.log2(M+N-1)))
+    # generate values of 'y'
+    # y = A ^ (-n) * W ^ (n**2 / 2) * x
+    # Y = fft(Y padded to power of two)
+    n = np.arange(N,dtype=float)
+    y = np.power(A,-n) * np.power(W,n**2 / 2.) * x
+    Y = np.fft.fft(y,L)
 
-	# generate values of 'y'
-	# y = A ^ (-n) * W ^ (n**2 / 2) * x
-	# Y = fft(Y padded to power of two)
-	n = np.arange(N,dtype=float)
-	y = np.power(A,-n) * np.power(W,n**2 / 2.) * x
-	Y = np.fft.fft(y,L)
+    v = np.zeros(L,dtype=np.complex)
+    v[:M] = np.power(W,-n[:M]**2/2.)
+    v[L-N+1:] = np.power(W,-n[N-1:0:-1]**2/2.)
+    V = np.fft.fft(v)
 
-	v = np.zeros(L,dtype=np.complex)
-	v[:M] = np.power(W,-n[:M]**2/2.)
-	v[L-N+1:] = np.power(W,-n[N-1:0:-1]**2/2.)
-	V = np.fft.fft(v)
+    # fast convolve, undo, select first M samples
+    g = np.fft.ifft(V*Y)[:M]
+    k = np.arange(M)
+    g *= np.power(W,k**2 / 2.)
 
-	# fast convolve, undo, select first M samples
-	g = np.fft.ifft(V*Y)[:M]
-	k = np.arange(M)
-	g *= np.power(W,k**2 / 2.)
-
-	return g
+    return g
--- test.py
+++ test.py
@@ -1,19 +1,23 @@
 from chirpz import chirpz
 import numpy as np
+import matplotlib
+
+matplotlib.use('qt5agg')
 from matplotlib import pyplot as plt
 
 t = np.linspace(0, 1, 48e3)
-x = np.sin(2200.*t*2*np.pi)[0:512]
-x += np.sin(1000.*t*2*np.pi)[0:512]
-x += np.sin(1200.*t*2*np.pi)[0:512]
-x += np.sin(120.*t*2*np.pi)[0:512]
+freqs = [2200, 1200, 1000, 120]
+x = np.random.rand(512)/100
+for freq in freqs:
+    x += np.sin(freq*t*2*np.pi)[0:512]
 
-m = len(x)/4
+print(freqs)
+m = len(x)//4
 
 Fs = 48e3
 
 F1 = 0
-F2 = 4000
+F2 = 3000
 
 # input, start, step, length
 # def chirpz(x, A, W, M):
@@ -28,6 +32,7 @@ cztAmps = np.abs(
 cztFreqs = np.linspace(F1, F2, m)
 
 cztPeaks = [cztFreqs[i-1] for i in range(m) if cztAmps[i-2] < cztAmps[i-1] > cztAmps[i] if cztAmps[i] > max(cztAmps)*0.5]
+print(cztPeaks)
 
 plt.vlines(cztPeaks, 0, max(cztAmps), 'r', label='peaks from czt')
 
@@ -38,7 +43,7 @@ fftFreqs = np.linspace(0, Fs/2, len(x)/2)
 fftAmps = np.abs(np.fft.rfft(x))[1::]
 
 fftPeaks = [fftFreqs[i-1] for i in range(m) if fftAmps[i-2] < fftAmps[i-1] > fftAmps[i] if fftAmps[i-1] > max(fftAmps)*0.5]
-print fftPeaks
+print(fftPeaks)
 plt.vlines(fftPeaks, 0, max(fftAmps), 'k', label='peaks from fft')
 
 plt.plot(fftFreqs, fftAmps, '.k', label='fft')
